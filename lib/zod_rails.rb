c277c926a277e62e26ca7933bb4e8bedea323ff2
Matt Kelly <matthew.ryan.kelly@gmail.com>
2026-01-13 19:23:38 -0500

    Initial gem scaffold from bundle gem

---
+++ lib/zod_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "zod_rails/version"
+
+module ZodRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/zod_rails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ZodRails
+  VERSION = "0.1.0"
+end
