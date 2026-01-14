f6a1d2038c6cd0627f1b7c4280bb9cdb780e2fff
Matt Kelly <matthew.ryan.kelly@gmail.com>
2026-01-13 19:24:38 -0500

    Wire up requires and module API

--- lib/zod_rails.rb
+++ lib/zod_rails.rb
@@ -1,8 +1,39 @@
 # frozen_string_literal: true
 
+require "logger"
 require_relative "zod_rails/version"
+require_relative "zod_rails/configuration"
+require_relative "zod_rails/mapping/type_mapper"
+require_relative "zod_rails/mapping/validation_mapper"
+require_relative "zod_rails/mapping/enum_mapper"
+require_relative "zod_rails/introspection/column_info"
+require_relative "zod_rails/introspection/validation_info"
+require_relative "zod_rails/introspection/model_inspector"
+require_relative "zod_rails/generation/schema_builder"
+require_relative "zod_rails/generation/typescript_emitter"
+require_relative "zod_rails/generation/file_writer"
+require_relative "zod_rails/generator"
 
 module ZodRails
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def logger
+      @logger ||= Logger.new($stdout, level: Logger::WARN)
+    end
+
+    attr_writer :logger
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+
+    def reset_configuration!
+      @configuration = Configuration.new
+    end
+  end
 end
--- spec/zod_rails_spec.rb
+++ spec/zod_rails_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ZodRails do
     expect(ZodRails::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "provides a generator" do
+    expect(ZodRails::Generator).to be_a(Class)
   end
 end
